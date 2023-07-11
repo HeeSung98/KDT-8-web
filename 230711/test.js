b8717b27d22faa8c55c7b6e7cc9ef050be03ba38
HeeSung98 <wlfkrdl2@gmail.com>
2023-07-11 17:02:36 +0900

    230711

---
+++ 230711/test.js
@@ -0,0 +1,5 @@
+let color1 = ['red', 'oragne', 'yello', 'green', 'blue', 'navy', 'purple']
+console.log(color1[2])
+color1.push('aqua')
+console.log(color1.indexOf('black'))
+console.log(color1.reverse())
