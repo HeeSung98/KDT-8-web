f146c9ea4ac459963de5a8dc3bab561e61086bed
HeeSung98 <wlfkrdl2@gmail.com>
2023-08-05 09:08:47 +0900

    230805

--- 230729/async_await.js
+++ 230729/async_await.js
@@ -5,19 +5,27 @@
 //     console.log(err)
 //   })
 
+let product
+let price
+let flag = false
 excute()
 
 function goMart() {
   console.log('마트도착')
 }
 
-function pickDrink() {
-  return new Promise(function (resolve, rejcet) {
+function pickDrink(money) {
+  return new Promise(function (resolve, reject) {
     setTimeout(function () {
       console.log('고민 끝!')
       product = '제로 콜라'
       price = 3000
-      resolve()
+      if (money >= 3000) {
+        flag = true
+        resolve()
+      } else {
+        reject('돈이 부족합니다.')
+      }
     }, 3000)
   })
 }
@@ -28,6 +36,10 @@ function pay() {
 
 async function excute() {
   goMart()
-  await pickDrink()
-  pay()
+  await pickDrink(4000).catch((잔액부족) => {
+    console.log(잔액부족)
+  })
+  if (flag) {
+    pay()
+  }
 }
