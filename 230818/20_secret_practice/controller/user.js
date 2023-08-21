6379622132ee5663fd48690bcd82bb4be5fe6677
HeeSung98 <wlfkrdl2@gmail.com>
2023-08-21 09:10:45 +0900

    230821

--- 230818/20_secret_practice/controller/user.js
+++ 230818/20_secret_practice/controller/user.js
@@ -1,5 +1,37 @@
-const { User } = require('../models');
+const { User } = require('../models')
+const bcrypt = require('bcrypt')
+
+const bcryption = (pw) => {
+  return bcrypt.hashSync(pw, 10)
+}
 
 exports.index = (req, res) => {
-    res.render('index');
-};
+  res.render('index')
+}
+
+exports.getRegister = (req, res) => {
+  res.render('register')
+}
+
+exports.getLogin = (req, res) => {
+  res.render('login')
+}
+
+exports.postRegister = async (req, res) => {
+  try {
+    const { userId, userName, userPw } = req.body
+    console.log('제대로 나오고있나?', userId, userName, userPw)
+    const hashPw = bcryption(userPw)
+    const result = await User.create({
+      userId,
+      userName,
+      userPw: hashPw,
+    })
+    if (result) {
+      res.json({ result: true })
+    }
+  } catch (e) {
+    console.log(e)
+    res.json({ result: false })
+  }
+}
--- 230818/20_secret_practice/routes/user.js
+++ 230818/20_secret_practice/routes/user.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controller/user');
+const express = require('express')
+const router = express.Router()
+const controller = require('../controller/user')
 
-router.get('/', controller.index);
+router.get('/', controller.index)
 //로그인
 //회원가입
+router.get('/register', controller.getRegister)
+router.post('/register', controller.postRegister)
 
-module.exports = router;
+module.exports = router
