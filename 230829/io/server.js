96dec3f8ef6cfb122a4c891f4a40bc1130d37c63
HeeSung98 <wlfkrdl2@gmail.com>
2023-08-29 12:27:18 +0900

    230809

---
+++ 230829/io/server.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const socketIo = require('socket.io')
+const express = require('express')
+const app = express()
+const PORT = 8080
+
+app.set('view engine', 'ejs')
+
+const server = http.createServer(app)
+const io = socketIo(server)
+
+app.get('/', (req, res) => {
+  res.render('client')
+})
+
+io.on('connection', (socket) => {
+  socket.on('new_message', (arg, cb) => {
+    console.log(arg)
+    cb(arg)
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`)
+  document.querySelector('#log').innerHTML = `<li>${res.name}: ${res.msg}</li>`
+})
